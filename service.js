716981e4ac701c60c9fbf0962f5806727760b1cd
anshuldatey04-lol <anshuldatey04@gmail.com>
2026-05-05 03:59:09 +0530

    Create service.js

---
+++ service.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = 'racket-controller-v1';
+const ASSETS = [
+  'index.html',
+  'js/controller-smartphone.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
